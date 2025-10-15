5082b50b70e72e8baf05360b2a429007125e5344
VinceMikel <vincemikelmasong724@gmail.com>
2025-10-14 23:24:06 -0400

    Update BubbleSort.java

--- src/main/java/com/mycompany/week8_skeletoncode_lab/BubbleSort.java
+++ src/main/java/com/mycompany/week8_skeletoncode_lab/BubbleSort.java
@@ -13,8 +13,12 @@ public class BubbleSort {
     public static void bubbleSort(int a[], int size) {
         int outer, inner, temp;
         for (outer = size - 1; outer > 0; outer--) { // counting down
-            for (inner = 0; inner < outer; inner++) { // bubbling up
-                //ToDo 3: complete this algorithm, test it, provide its time complexity
+            for (inner = 0; inner < outer; inner++) { // keeps going up for every iteration inner is smaller than the outter
+               if (a[inner] > a[inner + 1]) {    //this keeps bubbling up until the limits are reached        
+                    temp = a[inner]; //placeholder for the number
+                    a[inner] = a[inner + 1];  
+                    a[inner + 1] = temp; //inner index number goes up, and goes to the temp placeholder
+                }
             }
         }
     }
